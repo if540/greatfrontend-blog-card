10a19bf7130fcba2597cb8167d705109fc0de991
Kerwin <if5400@gmail.com>
2024-07-02 11:32:38 +0800

    update 調整成符合題目規格

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,13 @@ import blogCard from './components/blogCard.vue'
 html, body {
   width: 100%;
   height: 100%;
+  background: linear-gradient(147.52deg, #F9FAFB 8.89%, #D2D6DB 100.48%);
 }
 #app {
   width: 100%;
   height: 100%;
   display: flex;
-  justify-content: center; 
-  align-items: center;
-  background: linear-gradient(147.52deg, #F9FAFB 8.89%, #D2D6DB 100.48%);
+  justify-content: center;
+  align-items: flex-start;
 }
 </style>
--- src/components/blogCard.vue
+++ src/components/blogCard.vue
@@ -4,7 +4,7 @@
     <div class="card__bd">
       <span class="text-sm text-green-700 rounded-full card__tag fill-green-50 outline outline-2 outline-green-200">Interior</span>
       <h1 class="text-lg font-semibold card__title text-neutral-900">Top 5 Liveing Room Inspirations</h1>
-      <p class="text-base font-medium card__dec">Curated vibrants colors for your living, make it pop & calm in the same time.</p>
+      <p class="text-base font-medium line-clamp-2 card__dec">Curated vibrants colors for your living, make it pop & calm in the same time.</p>
       <button class="text-indigo-700 card__btn" aria-label="Read more about top 5 liveing room inspirations."><span aria-hidden="true">Read more</span><svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M13.4763 9.16658L9.00634 4.69658L10.1848 3.51807L16.6667 9.99992L10.1848 16.4817L9.00634 15.3032L13.4763 10.8332H3.33334V9.16658H13.4763Z" fill="#4338CA"/>
         </svg>
@@ -16,7 +16,7 @@
 <style lang="scss">
 .card {
   max-width: 340px;
-  margin: 0 auto;
+  margin: 120px auto 0;
   font-family: 'Noto Sans', Arial, Helvetica, sans-serif;
 
   // structure
