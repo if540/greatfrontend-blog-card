f52ca22c31eb9cc78b7c3799edbdf40ea04c62c5
Kerwin <if5400@gmail.com>
2024-07-02 10:54:17 +0800

    update [Stretch goal] Accessibility and semantics

--- src/components/blogCard.vue
+++ src/components/blogCard.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="card bg-white border-neutral-200 border-2 shadow overflow-hidden rounded-lg">
-      <div class="card__hd"><img src="/Image@2x.png" alt="田園風格客廳"></div>
-      <div class="card__bd">
-        <div class="card__tag text-sm text-green-700 fill-green-50 outline outline-2 outline-green-200 rounded-full">Interior</div>
-        <h1 class="card__title text-lg text-neutral-900 font-semibold">Top 5 Liveing Room Inspirations</h1>
-        <p class="card__dec text-base font-medium">Curated vibrants colors for your living, make it pop & calm in the same time.</p>
-        <button class="card__btn text-indigo-700" ><span>Read more</span><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M13.4763 9.16658L9.00634 4.69658L10.1848 3.51807L16.6667 9.99992L10.1848 16.4817L9.00634 15.3032L13.4763 10.8332H3.33334V9.16658H13.4763Z" fill="#4338CA"/>
-          </svg>
-        </button>
-      </div>
+  <div class="overflow-hidden bg-white border-2 rounded-lg shadow card border-neutral-200">
+    <figure class="card__hd"><img src="/Image@2x.png" alt="Countryside-style living room"></figure>
+    <div class="card__bd">
+      <span class="text-sm text-green-700 rounded-full card__tag fill-green-50 outline outline-2 outline-green-200">Interior</span>
+      <h1 class="text-lg font-semibold card__title text-neutral-900">Top 5 Liveing Room Inspirations</h1>
+      <p class="text-base font-medium card__dec">Curated vibrants colors for your living, make it pop & calm in the same time.</p>
+      <button class="text-indigo-700 card__btn" aria-label="Read more about top 5 liveing room inspirations."><span aria-hidden="true">Read more</span><svg aria-hidden="true" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M13.4763 9.16658L9.00634 4.69658L10.1848 3.51807L16.6667 9.99992L10.1848 16.4817L9.00634 15.3032L13.4763 10.8332H3.33334V9.16658H13.4763Z" fill="#4338CA"/>
+        </svg>
+      </button>
     </div>
+  </div>
 </template>
 
 <style lang="scss">
